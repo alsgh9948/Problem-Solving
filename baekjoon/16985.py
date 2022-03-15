a4d7ef5e9a3f6109dbae4263b3ca1ba140b6bb9c
seo minho <alsgh9948@naver.com>
2022-03-15 15:46:34 +0900

    0315 백준풀이

--- baekjoon/16985.py
+++ baekjoon/16985.py
@@ -1,89 +1,96 @@
 from sys import stdin
-from itertools import permutations
 from collections import deque
+from itertools import permutations
 
 input = stdin.readline
+INF = 9876543210
+
 dx = [-1,1,0,0,0,0]
 dy = [0,0,-1,1,0,0]
 dz = [0,0,0,0,-1,1]
-INF = 9876543210
 
-board = [[list(map(int, input().split())) for _ in range(5)] for _ in range(5)]
+origin_board = [[list(map(int, input().split())) for _ in range(5)] for _ in range(5)]
 visited = [[[0] * 5 for _ in range(5)] for _ in range(5)]
 visited_num = 0
 answer = INF
 
 def solv():
-    for order in permutations(range(5),5):
-        stack_board(order,0)
-    print(answer if answer != INF else -1)
-def stack_board(order,idx):
-    global board
-    if answer == 12:
-        return
-    if idx == 5:
-        simul(order)
-        return
-    for _ in range(4):
-        board_rotate(order[idx])
-        stack_board(order,idx+1)
-
-def simul(order):
-    global answer,visited,visited_num
-    game_board = []
-    for idx in order:
-        game_board.append(board[idx])
-
-    visited_num += 1
-
-    start = [0,0,0]
-    end = [4,4,4]
-
-    if game_board[start[0]][start[1]][start[2]] != 1 or game_board[end[0]][end[1]][end[2]] != 1:
-        return
-
-    visited_num += 1
-    q = deque([start+[0]])
-    visited[0][start[0]][start[1]] = visited_num
-    while q:
-        z,x,y,cnt = q.pop()
-
-        if cnt >= answer:
-            continue
-        if end[0] == z and end[1] == x and end[2] == y:
-            answer = min(answer,cnt)
+    for order in permutations(range(5)):
+        new_board = set_new_board(order)
+        make_board(0,new_board)
+        if answer == 12:
             break
 
-        for d in range(6):
+    print(answer if answer != INF else -1)
+def make_board(now,board):
+    if now == 5:
+        simul(board)
+        return
+    for _ in range(4):
+        board[now] = rotate_board(board[now])
+        if (now == 0 and board[0][0][0] == 0) or (now == 4 and board[4][4][4] == 0):
+            continue
+        else:
+            make_board(now + 1, board)
+            if answer == 12:
+                return
 
+def rotate_board(board):
+    new_board = []
+    for y in range(4,-1,-1):
+        row = []
+        for x in range(5):
+            row.append(board[x][y])
+        new_board.append(row)
+    return new_board
+
+def simul(board):
+    global visited, visited_num, answer
+    q = deque([(0,0,0,0)])
+
+    visited_num += 1
+    visited[0][0][0] = visited_num
+
+    while q:
+        z,x,y,cnt = q.pop()
+        if cnt == answer:
+            return
+
+        if (z,x,y) == (4,4,4):
+            answer = min(answer, cnt)
+            return
+
+        for d in range(6):
             nz = z + dz[d]
             nx = x + dx[d]
             ny = y + dy[d]
 
-            if point_validator(nz,nx,ny,game_board):
+            if point_validator(nz,nx,ny,board):
                 visited[nz][nx][ny] = visited_num
                 q.appendleft((nz,nx,ny,cnt+1))
-def point_validator(z,x,y,game_board):
+
+def point_validator(z,x,y,board):
     if z < 0 or x < 0 or y < 0 or z >= 5 or x >= 5 or y >= 5:
         return False
-    elif game_board[z][x][y] == 0:
+    elif board[z][x][y] == 0:
         return False
     elif visited[z][x][y] == visited_num:
         return False
     return True
 
-def board_rotate(z):
-    global board
+def set_new_board(order):
+    new_board = []
+    for idx in order:
+        new_board.append(copy_board(idx))
 
-    tmp = []
-    for row in board[z]:
-        tmp_row = []
-        for num in row:
-            tmp_row.append(num)
-        tmp.append(tmp_row)
-
+    return new_board
+def copy_board(idx):
+    new_board = []
     for x in range(5):
+        row = []
         for y in range(5):
-            board[z][y][4-x] = tmp[x][y]
+            row.append(origin_board[idx][x][y])
+        new_board.append(row)
+    return new_board
 
 solv()
--- baekjoon/17090.py
+++ baekjoon/17090.py
@@ -1,57 +1,62 @@
-from sys import stdin,setrecursionlimit
+from sys import stdin
+
+input = stdin.readline
 dir = {
-    'U':[-1,0],
-    'D':[1,0],
-    'R':[0,1],
-    'L':[0,-1]
+    'U':(-1,0),
+    'D':(1,0),
+    'L':(0,-1),
+    'R':(0,1)
 }
 
-n,m = map(int, stdin.readline().split())
+n,m = map(int, input().split())
+board = [input().strip() for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
+visited_num = 0
 
-board = [stdin.readline() for _ in range(n)]
-is_exit = [[0]*m for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
+is_possible = [[-1]*m for _ in range(n)]
 
 def solv():
-    ans = 0
-    for i in range(n):
-        for j in range(m):
-            if is_exit[i][j] == 0:
-                ans += start(i,j)
+    global visited_num
+    answer = 0
+    for sx in range(n):
+        for sy in range(m):
+            if is_possible[sx][sy] == -1:
+                visited_num += 1
+                answer += simul(sx,sy)
+            else:
+                answer += is_possible[sx][sy]
+    print(answer)
+def simul(x,y):
+    global visited, is_possible
 
-    return ans
-
-def start(sx,sy):
-    global is_exit, visited
-    path = [(sx,sy)]
-    visited[sx][sy] = True
-    rst = 0
-    cnt = 0
+    s = [(x,y)]
+    rst = -1
     while True:
-        x,y = path[-1]
-        d = board[x][y]
-        nx = x + dir[d][0]
-        ny = y + dir[d][1]
+        visited[x][y] = visited_num
 
-        if nx < 0 or ny < 0 or nx >= n or ny >= m:
+        d = board[x][y]
+        x += dir[d][0]
+        y += dir[d][1]
+
+
+        if not boundary_validator(x, y):
             rst = 1
             break
-        elif visited[nx][ny]:
-            rst = 2
-            break
         else:
-            if is_exit[nx][ny] == 0:
-                visited[nx][ny] = True
-                path.append((nx,ny))
-            else:
-                rst = is_exit[nx][ny]
+            s.append((x,y))
+            if is_possible[x][y] != -1:
+                rst = is_possible[x][y]
                 break
+            else:
+                if visited[x][y] == visited_num:
+                    rst = 0
+                    break
+    for x,y in s:
+        is_possible[x][y] = rst
+    return rst
+def boundary_validator(x,y):
+    if x < 0 or y < 0 or x >= n or y >= m:
+        return False
+    return True
 
-    for x,y in path:
-        is_exit[x][y] = rst
-        visited[x][y] = False
-        if rst == 1:
-            cnt += 1
-
-    return cnt
-print(solv())
+solv()
