25ec615c3b10c56d482ce562554a18fcaaae2f6d
Seo min ho <alsgh9948@naver.com>
2022-03-23 17:24:08 +0900

    문제풀이

--- baekjoon/21608.py
+++ baekjoon/21608.py
@@ -5,62 +5,65 @@ dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 n = int(input())
-student = []
+board = [[0]*n for _ in range(n)]
 
-for _ in range(n**2):
-    student.append(list(map(int, input().split())))
-
-board = [[[0,0] for _ in range(n)] for _ in range(n)]
+infos = {}
+for _ in range(n*n):
+    info = list(map(int, input().split()))
+    infos[info[0]] = info[1:]
 
 def solv():
-    global board
-    for idx in range(n**2):
-        num = student[idx][0]
-        like_list = student[idx][1:]
-        pos = [0,0,-1000,-1000]
-        for x in range(n):
-            for y in range(n):
-                if board[x][y][0] == 0:
-                    cnt,empty_cnt = check_position(x,y,like_list)
-                    pos = max(pos,[cnt,empty_cnt,-x,-y])
-
-        x,y = -pos[2],-pos[3]
-        board[x][y] = [num,idx]
-
-    print(calc_ans())
-
-def calc_ans():
-    ans = 0
+    for num, like in infos.items():
+        insert_num(num,like)
+    print(calc_total())
+def calc_total():
+    total = 0
     for x in range(n):
         for y in range(n):
-            like_list = student[board[x][y][1]][1:]
-            cnt = 0
+            num = board[x][y]
+            like = infos[num]
+            like_cnt = 0
             for d in range(4):
                 nx = x + dx[d]
                 ny = y + dy[d]
-                if point_validator(nx, ny):
-                    num = board[nx][ny][0]
-                    if num in like_list:
-                        cnt += 1
-            if cnt != 0:
-                ans += 10**(cnt-1)
-    return ans
-def check_position(x,y,like_list):
-    cnt = 0
-    empty_cnt = 0
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if point_validator(nx,ny):
-            num = board[nx][ny][0]
-            if num == 0:
-                empty_cnt += 1
-            elif num in like_list:
-                cnt += 1
-    return cnt,empty_cnt
-def point_validator(x,y):
+
+                if boundary_validator(nx, ny) and board[nx][ny] in like:
+                    like_cnt += 1
+            if like_cnt > 0:
+                total += 10**(like_cnt-1)
+    return total
+def insert_num(num,like):
+    global board
+
+    max_like_cnt = -1
+    max_empty_cnt = -1
+    tx,ty=-1,-1
+    for x in range(n):
+        for y in range(n):
+            tmp_like_cnt = 0
+            tmp_empty_cnt = 0
+            if board[x][y] == 0:
+                for d in range(4):
+                    nx = x + dx[d]
+                    ny = y + dy[d]
+
+                    if boundary_validator(nx,ny):
+                        if board[nx][ny] == 0:
+                            tmp_empty_cnt += 1
+                        elif board[nx][ny] in like:
+                            tmp_like_cnt += 1
+                if max_like_cnt < tmp_like_cnt:
+                    max_like_cnt = tmp_like_cnt
+                    max_empty_cnt = tmp_empty_cnt
+                    tx,ty = x,y
+                elif max_like_cnt == tmp_like_cnt:
+                    if max_empty_cnt < tmp_empty_cnt:
+                        max_empty_cnt = tmp_empty_cnt
+                        tx,ty = x,y
+    board[tx][ty] = num
+
+def boundary_validator(x,y):
     if x < 0 or y < 0 or x >= n or y >= n:
         return False
     return True
-
 solv()
